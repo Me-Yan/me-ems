74795473ed68130c16791ad423b96e0e3c031227
Me-Yan <yanyanghong_work@163.com>
2018-12-25 09:07:41 +0800

    111

--- me-ems/ems-common/src/main/java/com/me/inner/util/CommonUtil.java
+++ me-ems/ems-common/src/main/java/com/me/inner/util/CommonUtil.java
@@ -1,8 +1,13 @@
 package com.me.inner.util;
 
+import com.me.inner.constant.CommonConstant;
+import com.me.inner.dto.PaginationDTO;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Me on 2018/9/4.
@@ -21,5 +26,32 @@ public class CommonUtil {
         logger.debug("Execute Method matches...");
 
         return new BCryptPasswordEncoder(11).matches(rawPassword, encodedPassword);
+    }
+
+    public static PaginationDTO packagePagination(HttpServletRequest request) {
+        logger.debug("Execute Method packagePagination...");
+
+        PaginationDTO pagination = new PaginationDTO();
+
+        Integer curPage = 0;
+        Integer limit = CommonConstant.Pagination.DEFAULT_LIMIT;
+        Integer begin = 0;
+
+        String curPageStr = request.getParameter(CommonConstant.Pagination.CURRENT_PAGE);
+        if (StringUtils.isBlank(curPageStr)) {
+            curPage = Integer.valueOf(curPageStr);
+        }
+        String limitStr = request.getParameter(CommonConstant.Pagination.LIMIT);
+        if (StringUtils.isBlank(limitStr)) {
+            limit = Integer.valueOf(limitStr);
+        }
+
+        begin = (curPage*limit) + 1;
+
+        pagination.setCurPage(curPage);
+        pagination.setLimit(limit);
+        pagination.setBegin(begin);
+
+        return pagination;
     }
 }
